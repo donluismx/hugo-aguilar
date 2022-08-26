a85a42ba9c2f99368ff99127da0cc1fbd0572fbf
Luis <donluismx@gmail.com>
2022-08-26 18:10:25 -0500

    Header working progress

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,21 +1,23 @@
-import haHexImage from '../assets/images/ha-hexagon.png';
+import styles from './Header.module.css';
+import gcss from '../../App.module.css';
+import haHexImage from '../../assets/images/ha-hexagon.png';
+
 
 const Header = () => {
   return (
     <>
+    <div className="uk-container">
     <header>
       <nav></nav>
-      <div className={styles["header-badge"]}>
-        <div className={styles["header-image"]}>
-          <img src={haHexImage} />
-        </div>
+      <div className={`uk-align-center uk-margin-remove-top uk-width-1-1 uk-grid uk-child-width-1-1 uk-child-width-1-2@xl uk-child-width-1-2@l uk-child-width-1-2@m uk-child-width-1-2@s ${styles["header-badge"]}`}>
+        <img src={haHexImage} />
         <div className={styles["header-text"]}>
-          <h1>Planeación Estratégica<br /> y Servicios de<br /> Construcción</h1>
-          <h2>Arquitecto / Project<br /> Management</h2>
+          <h1 className={gcss['c-00194c']}>Planeación Estratégica<br /> y Servicios de<br /> Construcción</h1>
+          <h2 className={gcss['c-014067']}>Arquitecto / Project<br /> Management</h2>
         </div>
       </div>
     </header>
-
+    </div>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+import "uikit/dist/css/uikit.min.css";
+import "uikit/dist/js/uikit.min.js";
+
+import "locomotive-scroll/dist/locomotive-scroll.min.css";
+import "locomotive-scroll/dist/locomotive-scroll.min.js";
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
