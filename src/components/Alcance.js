43556ba8eb40cc773998eec1b3472c4678fb50f8
Luis <donluismx@gmail.com>
2022-08-30 17:43:58 -0500

    Alcance first approach.

--- src/components/Alcance.js
+++ src/components/Alcance.js
@@ -11,17 +11,17 @@ const Alcance = () => {
         </div>
       <div className={styles.table}>
         <div className={styles.arrowsContainer}>
-          <div className={`${styles.arrowHead} ${styles['bc-grimson']} `}><p>Some title and stuff</p></div>
-          <div className={`${styles.arrowHead} ${styles['bc-violet']} `}><p>Some title and stuff</p></div>
-          <div className={`${styles.arrowHead} ${styles['bc-teal']} `}><p>Some title and stuff</p></div>
-          <div className={`${styles.arrowHead} ${styles['bc-olive']} `}><p>Some title and stuff</p></div>
-          <div className={`${styles.arrowHead} ${styles['bc-sand']} `}><p>Some title and stuff</p></div>
+          <div className={`${styles.arrowHead} ${styles['bc-grimson']} `}><p>Project Management</p></div>
+          <div className={`${styles.arrowHead} ${styles['bc-violet']} `}><p>Arquitecto,<br /> Servicios de<br />construcción</p></div>
+          <div className={`${styles.arrowHead} ${styles['bc-teal']} `}><p>Director<br /> Responsable<br /> de Obra<br /> (DRO)</p></div>
+          <div className={`${styles.arrowHead} ${styles['bc-olive']} `}><p>Diseño y <br />Servicios de <br />Consultoría</p></div>
+          <div className={`${styles.arrowHead} ${styles['bc-sand']} `}><p>Servicios de <br />Valuación</p></div>
         </div>
         <div className="uk-panel">
           <table className={styles.tableData}>
             <thead></thead>
             <tr>
-              <td>
+              <td className={`${styles.tdDesc} ${styles['c-grimson']}`}>
                 <ul>
                   <li>Gestión de proyectos de infraestructura y recursos disponibles</li>
                   <li>Acciones oportunas/anticipar imprevistos o retrasos</li>
@@ -31,7 +31,7 @@ const Alcance = () => {
                   <li>Puesta en marcha</li>
                 </ul>
               </td>
-              <td>
+              <td className={`${styles.tdDesc} ${styles['c-violet']}`}>
                 <ul>
                   <li>Concepto, Anteproyectos e Ingenierías</li>
                   <li>Admon. Construcción.</li>
@@ -41,7 +41,7 @@ const Alcance = () => {
                   <li>Entrega y puesta en marcha.</li>
                 </ul>
               </td>
-              <td>
+              <td className={`${styles.tdDesc} ${styles['c-teal']}`}>
                 <ul>
                   <li>Cumplir con reglamentos; Municipales, Estatales y Federales.</li>
                   <li>Firma de Director Responsable de Obra, ante ayuntamientos.</li>
@@ -49,7 +49,7 @@ const Alcance = () => {
                   <li>Cierre y entrega de obra.</li>
                 </ul>
               </td>
-              <td>
+              <td className={`${styles.tdDesc} ${styles['c-olive']}`}>
                 <ul>
                   <li>Architecture & Diseño</li>
                   <li>Ingenierías</li>
@@ -57,12 +57,39 @@ const Alcance = () => {
                   <li>Análisis de alternativas</li>
                 </ul>
               </td>
-              <td>
+              <td className={`${styles.tdDesc} ${styles['c-sand']}`}>
                 <ul>
                   <li>Determinación del valor de; inmuebles, instalación, intangibles.</li>
                   <li>Tipo de avaluó para inmuebles; Comercial o catastral</li>
                   <li>Conciliación con ayuntamientos por cobro de derechos</li>
                 </ul>
+              </td>
+            </tr>
+            <tr className={styles.trAqua}><td colSpan={5}>PARTICIPACIONES</td></tr>
+            <tr>
+              <td colSpan={5}>
+                <div className={styles.areas}>
+                  <div>
+                    <ul>
+                      <li>Naves Industriales</li>
+                      <li>Casa Habitación</li>
+                      <li>Educación</li>
+                    </ul>
+                  </div>
+                  <div>
+                    <ul>
+                      <li>Comercio</li>
+                      <li>Servicios</li>
+                      <li>Restauración</li>
+                    </ul>
+                  </div>
+                  <div>
+                    <ul>
+                      <li>Almacenaje</li>
+                      <li>Agua Drenaje</li>
+                    </ul>
+                  </div>
+                </div>
               </td>
             </tr>
           </table>
