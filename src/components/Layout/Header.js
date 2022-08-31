2195a8bc8c84e865db72d4cbcfe7f95161cd75a5
Luis <donluismx@gmail.com>
2022-08-31 13:44:57 -0500

    Separation of the header as a layout component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,6 @@
+import Logo from '../Logo';
+
 import styles from './Header.module.css';
-import gcss from '../../App.module.css';
-import haHexImage from '../../assets/images/ha-hexagon.png';
 
 
 const Header = () => {
@@ -9,13 +9,7 @@ const Header = () => {
     <div className="uk-container">
     <header>
       <nav></nav>
-      <div className={`uk-align-center uk-margin-remove-top uk-width-1-1 uk-grid uk-child-width-1-1 uk-child-width-1-2@xl uk-child-width-1-2@l uk-child-width-1-2@m uk-child-width-1-2@s ${styles["header-badge"]}`}>
-        <img src={haHexImage} />
-        <div className={styles["header-text"]}>
-          <h1 className={gcss['c-00194c']}>Planeación Estratégica<br /> y Servicios de<br /> Construcción</h1>
-          <h2 className={gcss['c-014067']}>Arquitecto / Project<br /> Management</h2>
-        </div>
-      </div>
+      <Logo />
     </header>
     </div>
     </>
