9710f74fff91c871934d4e22ed0bed52fcef5033
donluismx <donluismx@gmail.com>
2022-09-25 18:48:28 -0500

    Typo of class

--- src/components/Resumen.js
+++ src/components/Resumen.js
@@ -10,7 +10,7 @@ const Resumen = () => {
       <h1 className={gcss['c-00194c']}>Líder de proyectos de infraestructura</h1>
       <div className="uk-grid uk-flex uk-flex-right">
         <div className="uk-width-1-1 uk-width-5-6@xl uk-width-5-6@l uk-width-5-6@m uk-width-1-1@s">
-          <div class="uk-panel">
+          <div className="uk-panel">
             <img className={`uk-align-left uk-margin-remove-adjacent ${styles.photo}`} src={haPhoto} width="150"  alt="Example image" />
             <h3 className={`${styles.h3} ${gcss['c-3f3f3f']}`}>Hugo L. Aguilar</h3>
             <h3 className={`${styles.h3} ${gcss['c-3f3f3f']}`}>Gestor de proyectos, analista de riesgos en el proceso de construcción.</h3>
@@ -29,17 +29,17 @@ const Resumen = () => {
           <div className="uk-width-1-1">
             <div className="uk-width-1-1 uk-child-width-1-2@xl uk-child-width-1-2@l uk-child-width-1-2@m uk-child-width-1-2@s uk-grid">
               <div>
-                <div class="uk-inline">
+                <div className="uk-inline">
                   <img src={sigsaImg} width="auto" alt="SIGsa Edificio de procesos ortográficos y topografía especializada, Tulantepec Hidalgo" />
-                  <div class={`uk-overlay uk-overlay-default uk-position-bottom ${styles.overlay}`}>
+                  <div className={`uk-overlay uk-overlay-default uk-position-bottom ${styles.overlay}`}>
                     <p><b>SIGsa Edificio de procesos ortográficos y topografía especializada.</b><br /> Tulantepec Hgo.</p>
                   </div>
                 </div>
               </div>
               <div>
-                <div class="uk-inline">
+                <div className="uk-inline">
                   <img src={sigsaImg} width="auto" alt="" />
-                  <div class={`uk-overlay uk-overlay-default uk-position-top ${styles.overlay}`}>
+                  <div className={`uk-overlay uk-overlay-default uk-position-top ${styles.overlay}`}>
                     <p><b>VWM, Project Management diseño y ejecución de Obra Civil proyecto TIguan.</b><br />
                     Cuautlancingo, Puebla</p>
                   </div>
