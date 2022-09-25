62b3363286ba17f03245440b491a097dedecbda9
donluismx <donluismx@gmail.com>
2022-09-25 18:51:03 -0500

    Fix in table

--- src/components/Alcance.js
+++ src/components/Alcance.js
@@ -20,78 +20,80 @@ const Alcance = () => {
         <div className="uk-panel">
           <table className={styles.tableData}>
             <thead></thead>
-            <tr>
-              <td className={`${styles.tdDesc} ${styles['c-grimson']}`}>
-                <ul>
-                  <li>Gestión de proyectos de infraestructura y recursos disponibles</li>
-                  <li>Acciones oportunas/anticipar imprevistos o retrasos</li>
-                  <li>Optimizar recursos a favor de proyecto</li>
-                  <li>Reportes de avance Es vs Debe.</li>
-                  <li>Gestión de Riesgos y Resiliencia</li>
-                  <li>Puesta en marcha</li>
-                </ul>
-              </td>
-              <td className={`${styles.tdDesc} ${styles['c-violet']}`}>
-                <ul>
-                  <li>Concepto, Anteproyectos e Ingenierías</li>
-                  <li>Admon. Construcción.</li>
-                  <li>Ing. de costos</li>
-                  <li>Construcción</li>
-                  <li>Admond. de recursos; tiempo, costo, calidad.</li>
-                  <li>Entrega y puesta en marcha.</li>
-                </ul>
-              </td>
-              <td className={`${styles.tdDesc} ${styles['c-teal']}`}>
-                <ul>
-                  <li>Cumplir con reglamentos; Municipales, Estatales y Federales.</li>
-                  <li>Firma de Director Responsable de Obra, ante ayuntamientos.</li>
-                  <li>Bitácora y reporte fotográfico</li>
-                  <li>Cierre y entrega de obra.</li>
-                </ul>
-              </td>
-              <td className={`${styles.tdDesc} ${styles['c-olive']}`}>
-                <ul>
-                  <li>Architecture & Diseño</li>
-                  <li>Ingenierías</li>
-                  <li>Definición de materiales, proceso constructivo, equipo, mano de obra, tiempos de entrega.</li>
-                  <li>Análisis de alternativas</li>
-                </ul>
-              </td>
-              <td className={`${styles.tdDesc} ${styles['c-sand']}`}>
-                <ul>
-                  <li>Determinación del valor de; inmuebles, instalación, intangibles.</li>
-                  <li>Tipo de avaluó para inmuebles; Comercial o catastral</li>
-                  <li>Conciliación con ayuntamientos por cobro de derechos</li>
-                </ul>
-              </td>
-            </tr>
-            <tr className={styles.trAqua}><td colSpan={5}>PARTICIPACIONES</td></tr>
-            <tr>
-              <td colSpan={5}>
-                <div className={styles.areas}>
-                  <div>
-                    <ul>
-                      <li>Naves Industriales</li>
-                      <li>Casa Habitación</li>
-                      <li>Educación</li>
-                    </ul>
+            <tbody>
+              <tr>
+                <td className={`${styles.tdDesc} ${styles['c-grimson']}`}>
+                  <ul>
+                    <li>Gestión de proyectos de infraestructura y recursos disponibles</li>
+                    <li>Acciones oportunas/anticipar imprevistos o retrasos</li>
+                    <li>Optimizar recursos a favor de proyecto</li>
+                    <li>Reportes de avance Es vs Debe.</li>
+                    <li>Gestión de Riesgos y Resiliencia</li>
+                    <li>Puesta en marcha</li>
+                  </ul>
+                </td>
+                <td className={`${styles.tdDesc} ${styles['c-violet']}`}>
+                  <ul>
+                    <li>Concepto, Anteproyectos e Ingenierías</li>
+                    <li>Admon. Construcción.</li>
+                    <li>Ing. de costos</li>
+                    <li>Construcción</li>
+                    <li>Admond. de recursos; tiempo, costo, calidad.</li>
+                    <li>Entrega y puesta en marcha.</li>
+                  </ul>
+                </td>
+                <td className={`${styles.tdDesc} ${styles['c-teal']}`}>
+                  <ul>
+                    <li>Cumplir con reglamentos; Municipales, Estatales y Federales.</li>
+                    <li>Firma de Director Responsable de Obra, ante ayuntamientos.</li>
+                    <li>Bitácora y reporte fotográfico</li>
+                    <li>Cierre y entrega de obra.</li>
+                  </ul>
+                </td>
+                <td className={`${styles.tdDesc} ${styles['c-olive']}`}>
+                  <ul>
+                    <li>Architecture & Diseño</li>
+                    <li>Ingenierías</li>
+                    <li>Definición de materiales, proceso constructivo, equipo, mano de obra, tiempos de entrega.</li>
+                    <li>Análisis de alternativas</li>
+                  </ul>
+                </td>
+                <td className={`${styles.tdDesc} ${styles['c-sand']}`}>
+                  <ul>
+                    <li>Determinación del valor de; inmuebles, instalación, intangibles.</li>
+                    <li>Tipo de avaluó para inmuebles; Comercial o catastral</li>
+                    <li>Conciliación con ayuntamientos por cobro de derechos</li>
+                  </ul>
+                </td>
+              </tr>
+              <tr className={styles.trAqua}><td colSpan={5}>PARTICIPACIONES</td></tr>
+              <tr>
+                <td colSpan={5}>
+                  <div className={styles.areas}>
+                    <div>
+                      <ul>
+                        <li>Naves Industriales</li>
+                        <li>Casa Habitación</li>
+                        <li>Educación</li>
+                      </ul>
+                    </div>
+                    <div>
+                      <ul>
+                        <li>Comercio</li>
+                        <li>Servicios</li>
+                        <li>Restauración</li>
+                      </ul>
+                    </div>
+                    <div>
+                      <ul>
+                        <li>Almacenaje</li>
+                        <li>Agua Drenaje</li>
+                      </ul>
+                    </div>
                   </div>
-                  <div>
-                    <ul>
-                      <li>Comercio</li>
-                      <li>Servicios</li>
-                      <li>Restauración</li>
-                    </ul>
-                  </div>
-                  <div>
-                    <ul>
-                      <li>Almacenaje</li>
-                      <li>Agua Drenaje</li>
-                    </ul>
-                  </div>
-                </div>
-              </td>
-            </tr>
+                </td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
@@ -99,5 +101,4 @@ const Alcance = () => {
     </>
   );
 }
- 
 export default Alcance;
